752daaeceb55683025e20f1ba3262674f4029089
zaratr93 <sarahtenorio2129@gmail.com>
2024-10-18 15:14:43 -0600

    inaseerrt

--- inser_ and_sort.py
+++ inser_ and_sort.py
@@ -9,3 +9,23 @@ d = [1, 2, 3]
 
 c.insert(0, d)
 print(c)
+
+
+#sort
+company = ["Mateo", "Juan", "Isabel"]
+company.sort()
+print(company)
+
+#(WHAT IF I TRY IT IN KOREAN???)
+bias = ["전호석", "민윤기", "김태형"]
+bias.sort()
+print(bias)
+
+#SI JALÓ EN COREANO PROFEEE 0_0
+
+#Reverse list
+company.sort(reverse=True)
+print(company)
+
+#Number of characters 
+company.sort(reverse=True, key=len)
